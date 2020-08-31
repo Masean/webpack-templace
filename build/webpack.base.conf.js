f5a9ff6bdc36c9b7637e5a41cef6b35b9fa8cf75
Andrey Morozov <am53am@yahoo.com>
2020-08-31 15:28:39 +0300

    Создаем отдельный vendors.js файл под все библиотеки

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -23,6 +23,18 @@ module.exports = {
         path: PATHS.dist,
         publicPath: '/'
     },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    name: 'vendor',
+                    test: /node_modules/,
+                    chunks: 'all',
+                    enforce: true
+                }
+            }
+        }
+    },
     module: {
         rules: [{
             test: /\.js$/,
