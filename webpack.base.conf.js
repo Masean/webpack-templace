8783f3d37cfa91ede7e40a6c767f9a391bb6be2a
Andrey Morozov <am53am@yahoo.com>
2020-08-31 10:57:21 +0300

    Подключим Webpack-merge - для разбивание  dev и build конфига

--- webpack.base.conf.js
+++ webpack.base.conf.js
@@ -1,13 +1,25 @@
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const PATHS = {
+    src: path.join(__dirname, './src'),
+    dist: path.join(__dirname, './dist'),
+    assets: 'assets/'
+  }
+  
+
 module.exports = {
+
+    externals: {
+        paths: PATHS
+    },
     entry: {
-        app: './src/index.js'
+        app: PATHS.src
     },
     output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, './dist'),
-        publicPath: '/dist'
+        filename: `${PATHS.assets}js/[name].js`,
+        path: PATHS.dist,
+        publicPath: '/'
     },
     module: {
         rules: [{
@@ -53,12 +65,10 @@ module.exports = {
            ]
         }]
     },
-    devServer: {
-        overlay: true
-    },
+
     plugins: [
         new MiniCssExtractPlugin({
-            filename: "[name].css"
+            filename: `${PATHS.assets}css/[name].css`
         })
     ]
 }
