c76648c3db2733a360ed04ea7f5b0f239cf1e40b
Andrey Morozov <am53am@yahoo.com>
2020-09-01 19:58:40 +0300

    настройка шаблонизатора pug

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,13 +1,18 @@
 const path = require('path')
+const fs = require('fs')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const  CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+//const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const PATHS = {
     src: path.join(__dirname, '../src'),
     dist: path.join(__dirname, '../dist'),
     assets: 'assets/'
   }
+
+  const PAGES_DIR = `${PATHS.src}/pug/pages/`
+  const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'))
   
 
 module.exports = {
@@ -37,6 +42,9 @@ module.exports = {
     },
     module: {
         rules: [{
+            test: /\.pug$/,
+            loader: 'pug-loader'
+          }, {
             test: /\.js$/,
             loader: 'babel-loader',
             exclude: '/node_modules/'
@@ -84,9 +92,6 @@ module.exports = {
                  loader: 'postcss-loader',
                  options: {sourceMap: true,
                            config: {path: `./postcss.config.js`}}
-               },{
-                 loader: 'sass-loader',
-                 options: {sourceMap: true}
                }
            ]
         }]
@@ -98,6 +103,7 @@ module.exports = {
     },
 
     plugins: [
+        //new CleanWebpackPlugin(PATHS.dist, {}),
         new MiniCssExtractPlugin({
             filename: `${PATHS.assets}css/[name].[contenthash].css`
         }),
@@ -112,6 +118,10 @@ module.exports = {
                 { from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts` },
                 { from: `${PATHS.src}/static`, to: "" }
             ]
-          })
+          }),
+          ...PAGES.map(page => new HtmlWebpackPlugin({
+            template: `${PAGES_DIR}/${page}`,
+            filename: `./${page.replace(/\.pug/,'.html')}`
+          }))
     ]
 }
